fd94f653a80ff8f4a738beb44875eb8a4ef7e5f8
dvltinv <dvltinv@gmail.com>
2024-12-16 06:18:29 +0500

    optim-js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-const menuIcon = document.querySelector(".menu-icon")
-const navbarNav = document.querySelector(".navbar-nav")
-const quitIcon = document.querySelector(".navbar-nav__icon")
-const scrollButton = document.querySelector(".back-to-top__container")
-const navbar = document.querySelector(".navbar")
+const menuIcon = document.querySelector(".menu-icon"),
+    navbarNav = document.querySelector(".navbar-nav"),
+    quitIcon = document.querySelector(".navbar-nav__icon"),
+    scrollButton = document.querySelector(".back-to-top__container"),
+    navbar = document.querySelector(".navbar"),
+    priceButton = document.querySelector(".price-button");
 
-
-
-menuIcon.addEventListener("click", () => {
-    navbarNav.style.display = "flex";
-})
-
-quitIcon.addEventListener("click", () => {
-    navbarNav.style.display = "none"
-})
-
+menuIcon.addEventListener("click", () => navbarNav.style.display = "flex");
+quitIcon.addEventListener("click", () => navbarNav.style.display = "none");
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 0) {
-        scrollButton.style.opacity = "1"
-        navbar.style.backgroundColor = "white";
-    }
-})
+    window.scrollY > 0 ? (scrollButton.style.opacity = "1", navbar.style.backgroundColor = "white") : null;
+});
+priceButton.addEventListener("click", () => alert("Message"));
